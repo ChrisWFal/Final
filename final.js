d917dd6cecc4a56336a3b7ca6ad8a440a9e70271
Chris <falkenbergch@mullenmail.com>
2016-02-18 00:30:33 -0700

    Final commit of project

--- final.js
+++ final.js
@@ -1,97 +1,68 @@
 var finalApp = angular.module('final', ['ngRoute']);
 
-finalApp.config(function($routeProvider) 
-    {
+finalApp.config(function ($routeProvider) {
     $routeProvider
-        .when('/', 
-              {
-              templateUrl : 'pages/home.html',
-			  })
-        .when('/flightSearch', 
-              {
-              templateUrl : 'pages/flightSearch.html',
-              controller  : 'flightController'
-              })
-        .when('/map',
-              {
-              templateUrl: 'pages/map.html',
-              controller : 'MainCtrl'
-              });
-	});
+        .when('/', {
+            templateUrl: 'pages/home.html',
+        })
+        .when('/carRental', {
+            templateUrl: 'pages/rental.html',
+            controller: 'rentalController'
+        })
+        .when('/map', {
+            templateUrl: 'pages/map.html',
+            controller: 'mapController'
+        });
+});
 
-finalApp.service("flightDestination", function(){
+finalApp.service("destination", function () {
     this.destination = "DIA";
 });
-finalApp.service("rentalCompany", function(){
-    this.company = "EUROCAR";
-});
 
 
-finalApp.controller('flightController', ['$scope', '$http', 'flightDestination', 'rentalCompany'    function($scope, $http, flightDestination, rentalCompany){
-    
-    $scope.destination = flightDestination.destination;
-    $scope.$watch('destination', function(){
-        flightDestination.destination = $scope.destination;
+
+finalApp.controller('rentalController', ['$scope', '$http', 'destination', function ($scope, $http, destination) {
+
+    $scope.destination = destination.destination;
+    $scope.$watch('destination', function () {
+        destination.destination = $scope.destination;
     });
-                                         
-    $scope.company = rentalCompany.company;
-    $scope.$watch('destination', function(){
-        rentalCompany.company = $scope.company;                                     
-    });
-    
-//    $scope.origin = flightOrigin.origin;
-//    $scope.$watch('origin', function(){
-//        flightOrigin.origin = $scope.origin;
-//    });
-//    
-//    $scope.duration = flightDuration.duration;
-//    $scope.$watch('duration', function(){
-//        flightDuration.duration = $scope.duration;
-//    });
-//    
-//    $scope.direct = flightDirect.direct;
-//    $scope.$watch('direct', function(){
-//        flightDirect.direct = $scope.direct;
-//    });
-//    
-//    $scope.maxPrice = flightMaxPrice.maxPrice;
-//    $scope.$watch('maxPrice', function(){
-//        flightMaxPrice.maxPrice = $scope.maxPrice;
-//    });
-    
-    
-  $scope.flightInfo = function(){
-      $scope.numResults;
-$http.get("https://api.sandbox.amadeus.com/v1.2/cars/search-airport?apikey=l912cgcrdidJdVHRdzD9Sp4JmUw7jDta&location="+$scope.destination+"&pick_up=2016-06-04&drop_off=2016-06-08")
-.success(function(result){  
-    console.log('yes');
-    $scope.flights = [];
-    
-    
-    for(var i = 0; i < result.results.length; i++)
-        {
-            $scope.flights.push(result.results[i]);
-            $scope.numResults = i;
-        }
-})
-.error(function(data, status){
-     $scope.numResult = 0;
-console.log('no');
-   
-});
+
+
+    $scope.rentalInfo = function () {
+        $scope.numResults;
+        $http.get("https://api.sandbox.amadeus.com/v1.2/cars/search-airport?apikey=uJPjGEi3CAx9nfAEoPu6ay2iqKyveDpp&location="+$scope.destination+"&pick_up=2016-06-04&drop_off=2016-06-08")
+            .success(function (result) {
+                console.log('yes');
+                $scope.rentals = [];
+
+
+                for (var i = 0; i < result.results.length; i++) {
+                    $scope.rentals.push(result.results[i]);
+                }
+            })
+            .error(function (data, status) {
+                $scope.numResult = 0;
+                console.log('no');
+
+            });
 
     };
 }]);
 
 var map;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
+    });
 }
 
-finalApp.controller('MainCtrl', function ($scope, $window) {
+finalApp.controller('map Controller', function ($scope, $window) {
     $window.map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: -34.397,
